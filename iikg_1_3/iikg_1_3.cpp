081c5ad38dfede287eb78836cf5a5d4881737ddf
Екатерина Челышкина <chelyshkinaekaterina@gmail.com>
2022-04-11 22:03:03 +0500

    обновлены комментарии

--- iikg_1_3/iikg_1_3.cpp
+++ iikg_1_3/iikg_1_3.cpp
@@ -13,7 +13,7 @@ static void RenderSceneCB()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3); //треугольник вместо вершины, 3 вершины вместо 1
+    glDrawArrays(GL_TRIANGLES, 0, 3); //треугольник вместо вершины, три вершины вместо одной
 
     glDisableVertexAttribArray(0);
 
@@ -56,7 +56,7 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    glClearColor(50.0f, 0.0f, 50.0f, 0.0f);
+    glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
 
     CreateVertexBuffer();
 
